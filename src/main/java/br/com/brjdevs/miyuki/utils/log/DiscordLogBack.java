754f9eb418274ed9a307f8ecd64d553b0806cecd
adriantodt <adriantodt@outlook.com>
2016-11-24 09:09:51 -0200

    [R] I think this will work...

--- src/main/java/br/com/brjdevs/miyuki/utils/log/DiscordLogBack.java
+++ src/main/java/br/com/brjdevs/miyuki/utils/log/DiscordLogBack.java
@@ -1,23 +1,40 @@
 package br.com.brjdevs.miyuki.utils.log;
 
 import br.com.brjdevs.miyuki.modules.cmds.PushCmd;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.PatternLayout;
+import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
 
 import static br.com.brjdevs.miyuki.utils.StringUtils.limit;
 
-public class DiscordLogBack extends AppenderBase<Object> {
+public class DiscordLogBack extends AppenderBase<ILoggingEvent> {
 	public static String latestLog = "";
+	private PatternLayout patternLayout;
 
 	@Override
-	protected void append(Object eventObject) {
+	public void start() {
+		patternLayout = new PatternLayout();
+		patternLayout.setContext(getContext());
+		patternLayout.setPattern("[%d{HH:mm:ss}] [%t/%level] [%logger{0}]: %msg%n");
+		patternLayout.start();
 
-		PushCmd.pushSimple("log", channel -> "**[LOG]** " + limit(eventObject.toString(), 1990));
+		super.start();
+	}
 
-		latestLog = latestLog.concat(eventObject.toString());
+	@Override
+	protected void append(ILoggingEvent event) {
+		if (!event.getLevel().isGreaterOrEqual(Level.DEBUG)) return;
+		
+		// Formata mensagem do log
+		String msg = patternLayout.doLayout(event);
+		PushCmd.pushSimple("log", channel -> "**[LOG]** " + limit(msg, 1990));
+
+		latestLog = latestLog.concat(msg);
 		if (latestLog.length() >= 30000) {
 			int cutAt = latestLog.indexOf('\n', 30000 - latestLog.length());
 			if (cutAt < 0) cutAt = 30001 - latestLog.length();
 			latestLog = latestLog.substring(cutAt);
 		}
 	}
-} 
+}
